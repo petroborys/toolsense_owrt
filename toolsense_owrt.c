c47143dbf26f175454d75b48eab27ec3b9bf8f65
petroborys <vagrant@vagrant.vm>
2019-08-07 17:00:04 +0000

    Add SSL and Timer

--- toolsense_owrt.c
+++ toolsense_owrt.c
@@ -1,26 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 #include <time.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdbool.h>
 #include <mosquitto.h>
+
+#define DELAY_SEND_TIME 5 //sec
+// #define PORT 10333
+#define PORT_SSL 20333
 
 char *topic = NULL;
 struct mosquitto *mosq = NULL;
 
-int mqtt_send(char *msg){
-  return mosquitto_publish(mosq, NULL, topic, strlen(msg), msg, 0, 0);
+bool send_mqtt_1m = false;
+
+void timer_handler_1m()
+{
+	send_mqtt_1m = true;
 }
- 
-int main(int argc, char *argv[])
+
+void timer_init()
+{
+	struct sigaction psa;
+	struct itimerval tv;
+
+	memset(&psa, 0, sizeof(psa));
+	psa.sa_handler = &timer_handler_1m;
+	sigaction(SIGALRM, &psa, NULL);
+
+
+	tv.it_interval.tv_sec = DELAY_SEND_TIME;
+	tv.it_interval.tv_usec = 0;
+	tv.it_value.tv_sec = DELAY_SEND_TIME;
+	tv.it_value.tv_usec = 0;
+	setitimer(ITIMER_REAL, &tv, NULL);
+}
+
+int mqtt_init()
 {
 	char *host = "m15.cloudmqtt.com";
-	int port = 10333;
+	int port = PORT_SSL;
 	int keepalive = 60;
 	bool clean_session = true;
+
+	int rc = 0;
 	
 	topic = "temp";
-	char *buf = malloc(64);
-	// char *buf = "Test";
 
 	mosquitto_lib_init();
 
@@ -33,6 +61,19 @@ int main(int argc, char *argv[])
 
 	mosquitto_username_pw_set(mosq, "thmcoslv", "odUivT2WEIsW");
 
+	rc = mosquitto_tls_set(mosq, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL, NULL, NULL);
+	if (rc) {
+		printf ("set ssl failed %d\n", rc);
+		return 1;
+	}
+
+	rc = mosquitto_tls_insecure_set(mosq, true);
+
+	if (rc) {
+		printf ("set insecure failed %d\n", rc);
+		return 1;
+	}	
+
 	if(mosquitto_connect(mosq, host, port, keepalive)){
 		printf("Unable to connect.\n");
 		return 1;
@@ -44,19 +85,48 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-    printf("\nMosquitto test\n\n");
+	return 0;
+}
 
-    time_t t;
-    time(&t);
-    printf("\n current time is : %s",ctime(&t));
-    sprintf(buf,"\n current time is : %s",ctime(&t));
+int mqtt_send(char *msg){
+  return mosquitto_publish(mosq, NULL, topic, strlen(msg), msg, 0, 0);
+}
 
-    int snd = mqtt_send(buf);
-    printf("mqtt_send error=%i\n", snd);
-    
-    // mosquitto_loop_forever(mosq, -1, 1);
+int send_time()
+{
+	char *buf = malloc(64);
+	time_t t;
 
-	mosquitto_destroy(mosq);
-	mosquitto_lib_cleanup();
-    return 0;
+	time(&t);
+	printf("\nCurrent time is: %s",ctime(&t));
+	sprintf(buf,"\nCurrent time is: %s",ctime(&t));
+
+	int rc = mqtt_send(buf);
+	printf("mqtt_send error=%i\n", rc);
+
+	return rc;
+}
+
+int main(int argc, char *argv[])
+{
+	printf("\nMosquitto test SSL\n\n");
+
+	mqtt_init();
+	timer_init();
+
+	while(1) {
+
+		if (send_mqtt_1m) {
+			send_time();
+			send_mqtt_1m = false;
+		}
+
+		sleep(1);
+
+	}
+
+	// mosquitto_loop_forever(mosq, -1, 1);
+	// mosquitto_destroy(mosq);
+	// mosquitto_lib_cleanup();
+	return 0;
 }
